d6392e42722d9047e3356103b03a033183fd6fd4
Nanret123 <nanretgungshik@gmail.com>
2023-11-07 10:55:23 +0100

    vercel deployment

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,42 +1,58 @@
-import React from 'react';
+import React from "react";
 import { motion } from "framer-motion";
 import "./Header.scss";
-import {images} from "../../constants";
-import { BiLogoFacebook } from "react-icons/bi";
+import { images } from "../../constants";
 import { AiOutlineTwitter, AiFillLinkedin, AiFillGithub } from "react-icons/ai";
 
 const Header = () => {
-	return (
-		<div id="Home" className="app__header app__flex">
-		<div className="app__header-content">
-      <motion.div
-			whileInView={{ x: [-100, 0], opacity: [0, 1]}}
-			transition={{ duration: 0.5}}
-			className="app__header-info" >
-									<p className="info-p1">Hello, I'm</p>
-					<h1 className="info-title">Nanret Gungshik</h1>
-					<p className="info-p2">And I am a <span> Backend Web Developer</span></p>
-					<div className="btn-container">
-						
-					</div>
-				
-        <div className="social-media">
-				<a href="https://twitter.com/_nan_ret_" target="_blank" rel="noreferrer"><AiOutlineTwitter /></a>
-				<a href="https://github.com/Nanret123" target="_blank" rel="noreferrer"><AiFillGithub  rel="noreferrer"/></a>
-				<a href="www.linkedin.com/in/nanret-gungshik-37a146216" target="_blank"><AiFillLinkedin rel="noreferrer" /></a>
-			</div>
-        <a href="#" target="_blank" className="app__header-btn">Download CV</a>
-			</motion.div>
-			
-			</div>
-      
-      <div className="app__header-img">
-      	<img src={images.profile_pic} alt="profile-img" />
-      </div>
-			
-			
-		</div>
-	)
-}
+  return (
+    <div id="Home" className="app__header app__flex">
+      <div className="app__header-content">
+        <motion.div
+          whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+          transition={{ duration: 0.5 }}
+          className="app__header-info"
+        >
+          <p className="info-p1">Hello, I'm</p>
+          <h1 className="info-title">Nanret Gungshik</h1>
+          <p className="info-p2">
+            And I am a <span> Backend Web Developer</span>
+          </p>
+          <div className="btn-container"></div>
 
-export default Header
+          <div className="social-media">
+            <a
+              href="https://twitter.com/_nan_ret_"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <AiOutlineTwitter />
+            </a>
+            <a
+              href="https://github.com/Nanret123"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <AiFillGithub rel="noreferrer" />
+            </a>
+            <a
+              href="www.linkedin.com/in/nanret-gungshik-37a146216"
+              target="_blank"
+            >
+              <AiFillLinkedin rel="noreferrer" />
+            </a>
+          </div>
+          <a href="#" target="_blank" className="app__header-btn">
+            Download CV
+          </a>
+        </motion.div>
+      </div>
+
+      <div className="app__header-img">
+        <img src={images.profile_pic} alt="profile-img" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/container/Skills/Skills.jsx
+++ src/container/Skills/Skills.jsx
@@ -1,45 +1,43 @@
-import React from 'react';
+import React from "react";
 import "./Skills.scss";
 import { motion } from "framer-motion";
-import {images} from "../../constants";
+import { images } from "../../constants";
 
 const Skills = () => {
-  
   const skills = [
-  	{ name: "React", icon: images.react},
-  	{ name: "Redux", icon: images.redux},
-  	{ name: "Javascript", icon: images.javascript},
-  	{ name: "Sass", icon: images.sass},
-  	{ name: "MaterialUi", icon: images.mu5},
-    {name: "Git", icon: images.git},
-    { name: "nodejs", icon: images.node},
-  	{ name: "MongoDB", icon: images.mongodb},
-  	
-  	];
+    { name: "React", icon: images.react },
+    { name: "Redux", icon: images.redux },
+    { name: "Javascript", icon: images.javascript },
+    { name: "Sass", icon: images.sass },
+    { name: "MaterialUi", icon: images.mu5 },
+    { name: "Git", icon: images.git },
+    { name: "nodejs", icon: images.node },
+    { name: "MongoDB", icon: images.mongodb },
+  ];
 
-	return (
-		<div className="app__skills" id="Skills">
-			<h2 className="head-text"> Skills</h2>
+  return (
+    <div className="app__skills" id="Skills">
+      <h2 className="head-text"> Skills</h2>
 
-			<div className="skills__container">
-			<motion.div className="skills__list">
-				{skills.map((skill) => (
-					<motion.div
-				 whileInView={{ opacity: [0, 1] }}
+      <div className="skills__container">
+        <motion.div className="skills__list">
+          {skills.map((skill) => (
+            <motion.div
+              whileInView={{ opacity: [0, 1] }}
               transition={{ duration: 0.5 }}
               className="skills__item app__flex"
-              key={skill.name}>
-					  <div className="app__flex">
-					  	<img src={skill.icon} alt={skill.name} />
-					  </div>
-					  <p className="p-text">{skill.name}</p>
-				</motion.div>
-					))}
-			</motion.div>
-				
-			</div>
-		</div>
-	)
-}
+              key={skill.name}
+            >
+              <div className="app__flex">
+                <img src={skill.icon} alt={skill.name} />
+              </div>
+              <p className="p-text">{skill.name}</p>
+            </motion.div>
+          ))}
+        </motion.div>
+      </div>
+    </div>
+  );
+};
 
-export default Skills
+export default Skills;
